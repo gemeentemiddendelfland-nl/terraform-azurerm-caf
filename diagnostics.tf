0d942dac99b188185d32c50ed3f5a7842fc330e5
Frank Groenewoud <fgroenewoud@gemeentewestland.nl>
2024-11-11 16:13:30 +0100

    added private_dns and virtual_subnets

--- diagnostics.tf
+++ diagnostics.tf
@@ -29,8 +29,10 @@ module "diagnostic_storage_accounts" {
   global_settings   = local.global_settings
   client_config     = local.client_config
   storage_account   = each.value
+  private_dns       = local.combined_objects_private_dns
   private_endpoints = try(each.value.private_endpoints, {})
   vnets             = local.combined_objects_networking
+  virtual_subnets   = local.combined_objects_virtual_subnets
 
   resource_group      = local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)]
   resource_group_name = can(each.value.resource_group.name) || can(each.value.resource_group_name) ? try(each.value.resource_group.name, each.value.resource_group_name) : null
