5044698175d9477c038ef59c58f55e8809f0fb67
Frank Groenewoud <fgroenewoud@gemeentewestland.nl>
2024-11-12 10:54:00 +0100

    removed vnets, added private_dns

--- diagnostics.tf
+++ diagnostics.tf
@@ -29,9 +29,8 @@ module "diagnostic_storage_accounts" {
   global_settings   = local.global_settings
   client_config     = local.client_config
   storage_account   = each.value
-  #private_dns       = local.combined_objects_private_dns
+  private_dns       = local.combined_objects_private_dns
   private_endpoints = try(each.value.private_endpoints, {})
-  vnets             = local.combined_objects_networking
   #virtual_subnets   = local.combined_objects_virtual_subnets
 
   resource_group      = local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)]
